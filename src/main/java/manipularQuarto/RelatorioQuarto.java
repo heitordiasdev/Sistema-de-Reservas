c031b79e2f1c1535346e604730a15b5efdcef2ef
Carlos César <91580777+carloscz17@users.noreply.github.com>
2023-12-12 22:17:16 -0300

    Update RelatorioQuarto.java

--- src/main/java/manipularQuarto/RelatorioQuarto.java
+++ src/main/java/manipularQuarto/RelatorioQuarto.java
@@ -8,21 +8,17 @@ public class RelatorioQuarto {
     public static void gerarRelatorio(List<Quarto> todosQuartos, Map<Periodo, Quarto> reservas) {
         System.out.println("Relatório de Quartos\n");
 
-        // Quartos Disponíveis
         List<Quarto> quartosDisponiveis = calcularQuartosDisponiveis(todosQuartos, reservas);
         System.out.println("Quartos Disponíveis:");
         exibirInformacoesQuartos(quartosDisponiveis);
 
-        // Quartos Indisponíveis
         List<Quarto> quartosIndisponiveis = calcularQuartosIndisponiveis(todosQuartos, reservas);
         System.out.println("\nQuartos Indisponíveis:");
         exibirInformacoesQuartos(quartosIndisponiveis);
 
-        // Preços de Cada Quarto
         System.out.println("\nPreços de Cada Quarto:");
         exibirPrecosQuartos(todosQuartos);
 
-        // Hóspedes
         System.out.println("\nHóspedes:");
         exibirHospedes(reservas);
     }
@@ -30,8 +26,6 @@ public class RelatorioQuarto {
     private static List<Quarto> calcularQuartosDisponiveis(List<Quarto> todosQuartos, Map<Periodo, Quarto> reservas) {
         GerenciadorQuarto gerenciadorQuarto = new GerenciadorQuarto();
         BuscarQuarto busca = new BuscarQuarto("2023-01-01", "2023-01-05", 2, 0);
-
-        // Filtrar apenas os quartos com status true (disponíveis)
         return gerenciadorQuarto.buscarQuartos(busca, reservas, todosQuartos)
                 .stream()
                 .filter(Quarto::isStatus)
@@ -39,7 +33,6 @@ public class RelatorioQuarto {
     }
 
     private static List<Quarto> calcularQuartosIndisponiveis(List<Quarto> todosQuartos, Map<Periodo, Quarto> reservas) {
-        // Filtrar apenas os quartos com status false (indisponíveis)
         return todosQuartos.stream()
                 .filter(quarto -> !calcularQuartosDisponiveis(todosQuartos, reservas).contains(quarto))
                 .toList();
